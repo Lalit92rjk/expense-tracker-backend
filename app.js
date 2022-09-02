09d75bb815575b755e55fd0bb81cbb9d07a0f687
Lalit Rajak <lalit92rajak@gmail.com>
2022-09-02 09:39:09 +0530

    task18

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const fs= require('fs');
 const path = require('path');
+
 
 const express = require('express');
 var cors = require('cors')
+
 
 const sequelize = require('./util/database');
 const User = require('./models/users');
@@ -10,10 +13,15 @@ const Order = require('./models/orders');
 const Forgotpassword = require('./models/forgotpassword');
 const helmet= require('helmet');
 const compression= require('compression');
+const morgan= require('morgan');
 
 const userRoutes = require('./routes/user')
 const purchaseRoutes = require('./routes/purchase')
 const resetPasswordRoutes = require('./routes/resetpassword')
+
+const accessLogStream= fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+//const privateKey= fs.readFileSync('server.key');
+//const certificate= fs.readFileSync('server.cert');
 
 const app = express();
 const dotenv = require('dotenv');
@@ -27,6 +35,7 @@ app.use(cors());
 // app.use(bodyParser.urlencoded());  ////this is for handling forms
 app.use(express.json());  //this is for handling jsons
 
+
 app.use('/user', userRoutes)
 
 
@@ -36,6 +45,8 @@ app.use('/password', resetPasswordRoutes);
 
 app.use(helmet());
 app.use(compression());
+app.use(morgan('combined', {stream: accessLogStream}));
+
 
 
 
@@ -48,11 +59,21 @@ Order.belongsTo(User);
 User.hasMany(Forgotpassword);
 Forgotpassword.belongsTo(User);
 
-sequelize.sync()
+/*sequelize.sync()
     .then(() => {
         app.listen(process.env.PORT || 3000);
     })
     .catch(err => {
         console.log(err);
-    })
+    })*/
 
+sequelize
+// .sync({ force: true })
+.sync()
+    .then(() => {
+        // https.createServer({key: privateKey, cert: certificate}, app)        
+        app.listen(process.env.PORT || 3000);
+    })
+    .catch(err => {
+        console.log(err);
+    })
