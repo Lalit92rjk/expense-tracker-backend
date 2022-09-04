8e558289e4a90c6331072aff8b0ed73089c93a60
Lalit Rajak <lalit92rajak@gmail.com>
2022-09-04 11:06:06 +0530

    polled

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use('/password', resetPasswordRoutes);
 
 app.use((req,res)=>{
     console.log('urlll',req.url);
-    console.log('polled succeesfully');
+    console.log('polled succeesfully log');
     res.sendFile(path.join(__dirname, `public/${req.url}`));
 })
 
